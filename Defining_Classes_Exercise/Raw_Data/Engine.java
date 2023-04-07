6d98c1a349296d7e7d139f6b8059725a05eba0db
denitsadonova <denitsadonova@gmail.com>
2023-04-07 11:46:53 +0100

    Exercise

---
+++ Defining_Classes_Exercise/Raw_Data/Engine.java
@@ -0,0 +1,17 @@
+package Exercise.Raw_Data;
+
+public class Engine {
+    private int engineSpeed;
+    private int enginePower;
+
+    public Engine(int engineSpeed, int enginePower) {
+        this.engineSpeed = engineSpeed;
+        this.enginePower = enginePower;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+
+}
