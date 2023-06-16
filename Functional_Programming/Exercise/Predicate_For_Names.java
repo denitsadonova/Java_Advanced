f92eae5e127b65101257435a466e6c69ebdaed12
denitsadonova <denitsadonova@gmail.com>
2023-06-16 12:44:50 +0100

    Create Predicate_For_Names.java

---
+++ Functional_Programming/Exercise/Predicate_For_Names.java
@@ -0,0 +1,20 @@
+package Exercise;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class Predicate_For_Names {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = Integer.parseInt(scanner.nextLine());
+        String[] names = scanner.nextLine().split("\\s+");
+
+        Predicate<String> checkValidLength = name -> name.length() <= n;
+
+        Arrays.stream(names).filter(checkValidLength)
+                .forEach(System.out::println);
+
+    }
+}
