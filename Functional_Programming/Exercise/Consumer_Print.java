9fa5440571a71a84bf89340a93fc07f7c6482ccd
denitsadonova <denitsadonova@gmail.com>
2023-06-16 12:43:27 +0100

    Create Consumer_Print.java

---
+++ Functional_Programming/Exercise/Consumer_Print.java
@@ -0,0 +1,24 @@
+package Exercise;
+
+
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+    public class Consumer_Print {
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            String[] names = input.split("\\s+");
+
+
+            Consumer<String[]> printArray = array -> {
+                for (String name : array) {
+                    System.out.println(name);
+                }
+            };
+
+            printArray.accept(names);
+
+        }
+    }
